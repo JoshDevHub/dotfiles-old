5eac809cf7003f552fa348b4e894d36f6d885a29
Josh Smith <jmsmith1018@gmail.com>
2023-04-14 11:12:21 -0600

    Add overmind to PATH

--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -18,4 +18,10 @@ export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
+# go
+export PATH=$PATH:/usr/local/go/bin
+
+# overmind
+export PATH="/home/josh/go/bin:$PATH"
+
 export PATH="$HOME/.local/bin:$PATH"
