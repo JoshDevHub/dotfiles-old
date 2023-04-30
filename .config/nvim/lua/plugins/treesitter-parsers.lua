a1f8fa766b0094dedbf2ca382499ad4e7e4567fd
Josh Smith <jmsmith1018@gmail.com>
2023-04-29 23:47:23 -0600

    Adjust treesitter config

---
+++ .config/nvim/lua/plugins/treesitter-parsers.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "embedded_template",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "ruby",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,10 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "embedded_template",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "ruby",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-    },
+    dependencies = { "RRethy/nvim-treesitter-endwise" },
+    opts = function(_, opts)
+      opts.endwise = { enable = true }
+      opts.indent = { enable = true, disable = { "yaml", "ruby" } }
+    end,
   },
 }
