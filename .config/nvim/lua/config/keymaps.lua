44f20f9dc9644437eae9d4afc4b64101d68c63ad
Josh Smith <jmsmith1018@gmail.com>
2023-05-02 23:55:55 -0600

    Add keymaps for gitsigns commands

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local opts = { noremap = true, silent = true }
+-- local opts = { noremap = true, silent = true }
+local function build_opts(desc)
+  local opts = { noremap = true, silent = true }
+  if desc then
+    opts["desc"] = desc
+  end
+
+  return opts
+end
+
 local keymap = vim.api.nvim_set_keymap
 
-keymap("i", "jk", "<ESC>", opts)
-keymap("i", "jj", "<ESC>", opts)
+keymap("i", "jk", "<ESC>", build_opts())
+keymap("i", "jj", "<ESC>", build_opts())
 
 vim.opt.timeoutlen = 300
+
+vim.keymap.set("n", "<leader>gl", require("gitsigns").blame_line, build_opts("git-blame"))
+vim.keymap.set("n", "<leader>gt", require("gitsigns").stage_hunk, build_opts("stage-hunk"))
