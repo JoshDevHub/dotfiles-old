e60abfc7431fc41859c7fd2f995e17b10aac8f17
Josh Smith <jmsmith1018@gmail.com>
2023-05-05 21:06:44 -0600

    Combine treesitter files

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,24 @@ return {
     opts = function(_, opts)
       opts.endwise = { enable = true }
       opts.indent = { enable = true, disable = { "yaml", "ruby" } }
+      opts.ensure_installed = {
+        "bash",
+        "embedded_template",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "ruby",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      }
     end,
   },
 }
