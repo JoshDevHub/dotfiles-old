186bbe4d6996d20edd545f11d18d44c79631ddf3
Josh Smith <jmsmith1018@gmail.com>
2023-10-20 23:43:00 -0700

    Add vim-tmux-navigator

---
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,9 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  keys = {
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+  },
+}
