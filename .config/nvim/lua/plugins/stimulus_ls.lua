34a16ed7b8041391956a6033da5214a75563cf91
Josh Smith <jmsmith1018@gmail.com>
2023-11-04 10:26:14 -0700

    Cleanup stimulus_ls config

---
+++ .config/nvim/lua/plugins/stimulus_ls.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        stimulus_ls = {
+          cmd = { "stimulus-language-server", "--stdio" },
+          filetypes = { "html", "ruby", "eruby" },
+        },
+      },
+    },
+  },
+}
